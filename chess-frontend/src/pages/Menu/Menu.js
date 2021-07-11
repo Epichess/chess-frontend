55a8a562766ac30f41cfdd3dedf73f396181d308
Thomas Krieg <71218509+ThomasKrg@users.noreply.github.com>
2021-07-11 21:03:36 +0200

    socket test

--- chess-frontend/src/pages/Menu/Menu.js
+++ chess-frontend/src/pages/Menu/Menu.js
@@ -3,11 +3,23 @@ import PropTypes from 'prop-types';
 import menuBackground from '../../Menu.png';
 import './Menu.scss'
 import {useHistory} from 'react-router-dom';
+import { io } from "socket.io-client";
 
 const Menu = () => {
+
+    const socket = io("http://localhost:8000");
+
     const history = useHistory();
     const handdlePlayBtnClick = () => {
         history.push('/Game');
+
+        socket.emit('create_game');
+        console.log('socket emit create-game');
+
+        socket.on("creating", (...args) => {
+            console.log('create and store game id')
+            // store game id
+          });
     }
     return (
         <div className="menuBackground">
